9196d88b40dca171ce16b45280ebe28b6107f000
Johan Bell <belljohan3@gmail.com>
2024-02-11 15:56:59 +0100

    Init Project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
